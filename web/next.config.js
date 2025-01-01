81c3b69b4d5109ae5fcfca91b7aea618ed6d36d6
Michael Lynn <merlynn@gmail.com>
2025-01-01 14:02:13 -0500

    bullshit

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/MacFaceSwap',
   images: { unoptimized: true },
   assetPrefix: './', // Relative paths
   basePath: '',
