a5bc3715f9b9d8f4fad82329377980442d3d7bdb
Michael Lynn <merlynn@gmail.com>
2025-01-01 12:26:26 -0500

    update next

--- web/next.config.js
+++ web/next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   basePath: '',
-  assetPrefix: '',
+  assetPrefix: './', // Add this line
+  trailingSlash: true // Add this line
 }
 
 module.exports = nextConfig
