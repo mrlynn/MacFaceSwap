5a9dbe6da8ba0d37c8c2e42d34474786435ec56d
Michael Lynn <merlynn@gmail.com>
2025-01-01 12:29:52 -0500

    update next

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  assetPrefix: isProd ? 'https://macfaceswap.com' : '',
   basePath: '',
-  assetPrefix: './', // Add this line
-  trailingSlash: true // Add this line
+  trailingSlash: true
 }
 
 module.exports = nextConfig
