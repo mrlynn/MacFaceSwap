c511c871ed26a9524d35249e028db7e7aa45ffad
Michael Lynn <merlynn@gmail.com>
2025-01-02 11:21:22 -0500

    rebuild again

--- web/next.config.js
+++ web/next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
-  basePath: '/MacFaceSwap',
-  assetPrefix: '/MacFaceSwap/',
+  // Since you're using a custom domain, we don't need basePath or assetPrefix
+  // as the assets will be served from the root
   optimizeFonts: false,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
